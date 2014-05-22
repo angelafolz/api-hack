292784ba95ab9935c33672459acdc6c909d4013c
angelafolz <angelafolz@gmail.com>
2014-05-22 17:47:12 -0500

    show apparatus name in categories

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
-function Category(name, image, feed){
-	this.name = name;
+function Category(apparatus, image, feed){
+	this.apparatus = apparatus;
 	this.image = image;
 	this.feed = feed;
 }
 
-var flyingtrapeze = new Category("Flying trapeze", "images/flyingtrapeze.jpg", "feed");
+var allCategories = {
+	flyingtrapeze: new Category("Flying trapeze", "images/flyingtrapeze.jpg", "feed"),
+	aerial: new Category("Aerial hoop", "images/hoop.png", "feed"),
+	germanwheel: new Category("German wheel", "images/germanwheel.jpg", "feed"),
+	handbalancing: new Category("Hand balancing", "images/handbalancing.jpg", "feed"),
+	firejuggling: new Category("Fire juggling", "images/firejuggling.png", "feed")
+};
 
 
 $(document).ready(function(){
@@ -12,11 +18,11 @@ $(document).ready(function(){
 	$(".categories li").click(function(){
 		$(".main").hide();
 		var catName = $(this).attr("class");
-		$(".category").addClass(catName).show();
+		$(".category").addClass(catName).prepend("<h1>" + allCategories[catName].apparatus + "</h1>").show();
 	});
 
 	$(".home").click(function(){
-		$(".category").hide().removeClass().addClass("category");
+		$(".category").hide().removeClass().addClass("category").children("h1").remove();
 		$(".main").show();
 	});
 
